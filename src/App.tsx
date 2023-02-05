8e70d6fc3db6b1194b87f025d615e865cc2d4caa
John Doe <local@local.local>
2023-02-05 13:48:23 +0200

    side inbetween fix

--- src/App.tsx
+++ src/App.tsx
@@ -96,44 +96,104 @@ function App() {
     }
   }
 
-  const checkRight = (col: number, tmpField: number, currPlayer: Player) => {
-    if (col < 3) {
-      console.log(`currPlayer: ${currPlayer}`)
-      console.log(`tmpField: ${tmpField}`)
-      let winCtr: number = 0
-      let winArr: number[] = [tmpField + 1, tmpField + 2, tmpField + 3]
-      if (currPlayer === Player.Yellow) {
-        winCtr = countWin(winArr, yellowArr)
+  const checkSide = (col: number, tmpField: number, currPlayer: Player) => {
+    console.log(`currPlayer: ${currPlayer}`)
+    console.log(`tmpField: ${tmpField}`)
+    let winCtr: number = 0
+    let winArr: number[] = []
+    let currArr: number[] = []
+    if (currPlayer === Player.Yellow) {
+      currArr = yellowArr
+    }
+    else {
+      currArr = redArr
+    }
+    if (col === 0) {
+      winArr = [tmpField + 1, tmpField + 2, tmpField + 3]
+    }
+    else if (col === 1) {
+      if (currArr.includes(tmpField - 1)) {
+        console.log('col == 1 && prevField set')
+        winArr = [tmpField - 1, tmpField + 1, tmpField + 2]
       }
-      else {
-        winCtr = countWin(winArr, redArr)
-      }
-      if (winCtr === 3) {
-        alert("WON RIGHT")
+      else if (currArr.includes(tmpField + 1)) {
+        console.log('col == 1 && prevField NOT set && nextField set')
+        winArr = [tmpField + 1, tmpField + 2, tmpField + 3]
       }
     }
-  }
-
-  const checkLeft = (col: number, tmpField: number, currPlayer: Player) => {
-    if (col > 2) {
-      console.log(`currPlayer: ${currPlayer}`)
-      console.log(`tmpField: ${tmpField}`)
-      let winCtr: number = 0
-      let winArr: number[] = [tmpField - 1, tmpField - 2, tmpField - 3]
-      if (currPlayer === Player.Yellow) {
-        winCtr = countWin(winArr, yellowArr)
+    else if (col === 2) {
+      if (currArr.includes(tmpField - 1)) {
+        console.log('col == 2 && prevField set')
+        if (currArr.includes(tmpField + 1)) {
+          console.log('col == 2 && prevField set && nextField set')
+          if (currArr.includes(tmpField + 2)) {
+            console.log('col == 2 && prevField set && nextField set && nextNextField set')
+            winArr = [tmpField - 1, tmpField + 1, tmpField + 2]
+          }
+        }
       }
-      else {
-        winCtr = countWin(winArr, redArr)
+      else if (currArr.includes(tmpField + 1)) {
+        console.log('col == 2 && prevField NOT set && nextField set')
+        winArr = [tmpField + 1, tmpField + 2, tmpField + 3]
       }
-      if (winCtr === 3) {
-        alert("WON LEFT")
+    }
+    else if (col === 3) {
+      if (currArr.includes(tmpField - 1)) {
+        console.log('col == 3 && prevField set')
+        if (currArr.includes(tmpField + 1)) {
+          console.log('col == 3 && prevField set && nextField set')
+          if (currArr.includes(tmpField + 2)) {
+            console.log('col == 3 && prevField set && nextField set && nextNextField set')
+            winArr = [tmpField - 1, tmpField + 1, tmpField + 2]
+          }
+          else {
+            console.log('col == 3 && prevField set && nextField set && nextNextField NOT set')
+            winArr = [tmpField - 2, tmpField - 1, tmpField + 1]
+          }
+        }
       }
+      else if (currArr.includes(tmpField + 1)) {
+        console.log('col == 3 && prevField NOT set && nextField set')
+        winArr = [tmpField + 1, tmpField + 2, tmpField + 3]
+      }
+    }
+    else if (col === 4) {
+      if (currArr.includes(tmpField + 1)) {
+        console.log('col == 4 && nextField set')
+        if (currArr.includes(tmpField - 1)) {
+          console.log('col == 4 && nextField set && prevField set')
+          if (currArr.includes(tmpField - 2)) {
+            console.log('col == 4 && nextField set && prevField set && prevPrevField set')
+            winArr = [tmpField - 2, tmpField - 1, tmpField + 1]
+          }
+        }
+      }
+      else if (currArr.includes(tmpField - 1)) {
+        console.log('col == 2 && nextField NOT set && prevField set')
+        winArr = [tmpField - 3, tmpField - 2, tmpField - 1]
+      }
+    }
+    else if (col === 5) {
+      if (currArr.includes(tmpField + 1)) {
+        console.log('col == 5 && nextField set')
+        winArr = [tmpField - 2, tmpField - 1, tmpField + 1]
+      }
+      else if (currArr.includes(tmpField - 1)) {
+        console.log('col == 5 && nextField NOT set && prevField set')
+        winArr = [tmpField - 3, tmpField - 2, tmpField - 1]
+      }
+    }
+    else if (col === 6) {
+      winArr = [tmpField - 3, tmpField - 2, tmpField - 1]
+    }
+    winCtr = countWin(winArr, currArr)
+    if (winCtr === 3) {
+      alert("WON ")
     }
   }
 
   const checkBottom = (row: number, tmpField: number, currPlayer: Player) => {
-    if (row <= 2) {
+    if (row < 3) {
       console.log(`currPlayer: ${currPlayer}`)
       console.log(`tmpField: ${tmpField}`)
       let winCtr: number = 0
@@ -186,8 +246,7 @@ function App() {
     console.log(redArr)
     console.log(yellowArr)
     checkBottom(row, tmpField, currPlayer)
-    checkLeft(col, tmpField, currPlayer)
-    checkRight(col, tmpField, currPlayer)
+    checkSide(col, tmpField, currPlayer)
     checkLowerLeftDiagonal(row, col, tmpField, currPlayer)
     checkLowerRightDiagonal(row, col, tmpField, currPlayer)
     checkUpperLeftDiagonal(row, col, tmpField, currPlayer)
