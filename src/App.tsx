17747b9b0d587b417e1b5dd882b71c8b1961c044
John Doe <local@local.local>
2023-02-08 01:52:19 +0200

    added returns

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import './App.css'
 import { useRef } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0)
+  const [gameActive, setGameActive] = useState<boolean>(true)
+
   const gridRef = useRef<HTMLDivElement>()
   const [redArr, updateRedArr] = useState<number[]>([])
   const [yellowArr, updateYellowArr] = useState<number[]>([])
@@ -31,44 +33,44 @@ function App() {
       case 0:
         if (row < 3 && currArr.includes(tmpField + 8) && currArr.includes(tmpField + 16) && currArr.includes(tmpField + 24)) {
           console.log(`[row == ${row} (row < 3)] && 3 diagonalDownRight set`)
-          alert(`${currPlayer} WON DIAGONAL RIGHT`)
+          return true
         }
-        break
+        return false
 
       case 1:
         if (row < 4 && currArr.includes(tmpField + 8) && currArr.includes(tmpField + 16)) {
           console.log(`[row == ${row} (row < 4)] && 2 diagonalDownRight set`)
           if (currArr.includes(tmpField - 8)) {
             console.log('diagonalTopLeft set && 2 diagonalDownRight set')
-            alert(`${currPlayer} WON DIAGONAL RIGHT`)
+            return true
           }
           if (currArr.includes(tmpField + 24)) {
             console.log('diagonalTopLeft NOT set && 3 diagonalDownRight set')
-            alert(`${currPlayer} WON DIAGONAL RIGHT`)
+            return true
           }
         }
-        break
+        return false
 
       case 2:
         if (row < 5 && currArr.includes(tmpField + 8)) {
           console.log(`[row == ${row} (row < 5)] && diagonalDownRight set`)
           if (currArr.includes(tmpField - 8)) {
-            console.log('diagonalTopLeft set')
+            console.log('(1st) diagonalDownRight set && (1st) diagonalTopLeft set')
             if (currArr.includes(tmpField + 16)) {
-              console.log(`2 diagonalDownRight set && diagonalTopLeft`)
-              alert(`${currPlayer} WON DIAGONAL RIGHT`)
+              console.log('(1st, 2nd) diagonalDownRight set && (1st) diagonalTopLeft set')
+              return true
             }
             if (currArr.includes(tmpField - 16)) {
-              console.log('(1st) diagonalDownRight set && (1st, 2nd) diagonalTopLeft set')
-              alert(`${currPlayer} WON DIAGONAL RIGHT`)
+              console.log('(1st) diagonalDownRight set && (1st, 2nd) diagonalTopLeft set && (2nd) diagonalDownRight NOT set')
+              return true
             }
           }
           if (currArr.includes(tmpField + 16) && currArr.includes(tmpField + 24)) {
-            console.log('(1st) diagonalDownRight NOT set && (1st, 2nd, 3rd) diagonalTopLeft set')
-            alert(`${currPlayer} WON DIAGONAL RIGHT`)
+            console.log('(1st, 2nd, 3rd) diagonalTopLeft set && (1st) diagonalDownRight NOT set')
+            return true
           }
         }
-        break
+        return false
 
       case 3:
         if (currArr.includes(tmpField - 8)) {
@@ -77,23 +79,23 @@ function App() {
             console.log('(1st, 2nd) diagonalTopLeft set')
             if (currArr.includes(tmpField - 24)) {
               console.log('(1st, 2nd, 3rd) diagonalTopLeft set')
-              alert(`${currPlayer} WON DIAGONAL RIGHT`)
+              return true
             }
             if (currArr.includes(tmpField + 8)) {
               console.log('(1st, 2nd) diagonalTopLeft set && (1st) diagonalDownRight set && (3rd) diagonalTopLeft NOT set')
-              alert(`${currPlayer} WON DIAGONAL RIGHT`)
+              return true
             }
           }
           if (currArr.includes(tmpField + 8) && currArr.includes(tmpField + 16)) {
             console.log('(1st) diagonalTopLeft set && (1st, 2nd) diagonalDownRight set && (2nd) diagonalTopLeft NOT set')
-            alert(`${currPlayer} WON DIAGONAL RIGHT`)
+            return true
           }
         }
         if (currArr.includes(tmpField + 8) && currArr.includes(tmpField + 16) && currArr.includes(tmpField + 24)) {
           console.log('(1st, 2nd, 3rd) diagonalDownLeft set && (1st) diagonalTopRight NOT set')
-          alert(`${currPlayer} WON DIAGONAL RIGHT`)
+          return true
         }
-        break
+        return false
 
       case 4:
         if (row > 0) {
@@ -104,41 +106,41 @@ function App() {
               console.log('(1st, 2nd) diagonalTopLeft set')
               if (currArr.includes(tmpField + 8)) {
                 console.log('(1st, 2nd) diagonalTopLeft && (1st) diagonalDownRight set')
-                alert(`${currPlayer} WON DIAGONAL RIGHT`)
+                return true
               }
             }
             if (currArr.includes(tmpField + 8) && currArr.includes(tmpField + 16)) {
               console.log('(1st) diagonalTopLeft set && (1st, 2nd) diagonalDownRight')
-              alert(`${currPlayer} WON DIAGONAL RIGHT`)
+              return true
             }
           }
           if (currArr.includes(tmpField + 8) && currArr.includes(tmpField + 16) && currArr.includes(tmpField + 24)) {
             console.log('diagonalTopLeft NOT set && (1st, 2nd, 3rd) diagonalDownRight set')
-            alert(`${currPlayer} WON DIAGONAL RIGHT`)
+            return true
           }
         }
-        break
+        return false
 
       case 5:
         if (row > 1 && currArr.includes(tmpField - 8) && currArr.includes(tmpField - 16)) {
           console.log(`[row == ${row} (row > 1)] && (1st, 2nd) diagonTopLeft set`)
           if (currArr.includes(tmpField + 8)) {
             console.log('(1st, 2nd) diagonalTopLeft set && (1st) diagonalDownRight set')
-            alert(`${currPlayer} WON DIAGONAL RIGHT`)
+            return true
           }
           if (currArr.includes(tmpField - 24)) {
             console.log('(1st) diagonalDownRight NOT set && (1st, 2nd, 3rd) diagonalTopLeft set')
-            alert(`${currPlayer} WON DIAGONAL RIGHT`)
+            return true
           }
         }
-        break
+        return false
 
       case 6:
         if (row > 2 && currArr.includes(tmpField - 8) && currArr.includes(tmpField - 16) && currArr.includes(tmpField - 24)) {
           console.log('[row == ${row} (row > 2)] && (1st, 2nd, 3rd) diagonalTopLeft set')
-          alert(`${currPlayer} WON DIAGONAL RIGHT`)
+          return true
         }
-        break
+        return false
     }
   }
 
@@ -159,23 +161,23 @@ function App() {
       case 0:
         if (row > 2 && currArr.includes(tmpField - 6) && currArr.includes(tmpField - 12) && currArr.includes(tmpField - 18)) {
           console.log(`row == ${row} (row > 2) && (1st, 2nd, 3rd) diagonalTopRight set`)
-          alert(`${currPlayer} WON DIAGONAL LEFT`)
+          return true
         }
-        break
+        return false
 
       case 1:
         if (row > 1 && currArr.includes(tmpField - 6) && currArr.includes(tmpField - 12)) {
           console.log(`row == ${row} (row > 1)`)
           if (currArr.includes(tmpField + 6)) {
             console.log('diagonalDownLeft set && 2 diagonalTopRight set')
-            alert(`${currPlayer} WON DIAGONAL LEFT`)
+            return true
           }
           if (currArr.includes(tmpField - 18)) {
             console.log(']diagonalDownLeft NOT set && 3 diagonalTopRight set')
-            alert(`${currPlayer} WON DIAGONAL LEFT`)
+            return true
           }
         }
-        break
+        return false
 
       case 2:
         if (row > 0 && currArr.includes(tmpField - 6)) {
@@ -184,19 +186,19 @@ function App() {
             console.log('&& diagonalDownLeft set')
             if (currArr.includes(tmpField + 12)) {
               console.log('diagonalDownLeft set && 2 diagonalTopRight set')
-              alert(`${currPlayer} WON DIAGONAL LEFT`)
+              return true
             }
             if (currArr.includes(tmpField - 12)) {
               console.log('diagonalDownLeft set && 2 diagonalTopRight set')
-              alert(`${currPlayer} WON DIAGONAL LEFT`)
+              return true
             }
           }
           if (currArr.includes(tmpField - 12) && currArr.includes(tmpField - 18)) {
             console.log('diagonalDownLeft NOT set && 3 diagonalTopRight set')
-            alert(`${currPlayer} WON DIAGONAL LEFT`)
+            return true
           }
         }
-        break
+        return false
 
       case 3:
         if (currArr.includes(tmpField - 6)) {
@@ -205,23 +207,23 @@ function App() {
             console.log('(1st, 2nd) diagonalTopRight set')
             if (currArr.includes(tmpField - 18)) {
               console.log('(1st, 2nd, 3rd) diagonalTopRight set')
-              alert(`${currPlayer} WON DIAGONAL LEFT`)
+              return true
             }
             if (currArr.includes(tmpField + 6)) {
               console.log('(1st, 2nd) diagonalTopRight set && (1st) diagonalDownLeft set')
-              alert(`${currPlayer} WON DIAGONAL LEFT`)
+              return true
             }
           }
           if (currArr.includes(tmpField + 6) && currArr.includes(tmpField + 12)) {
             console.log('(1st) diagonalTopRight set && (2nd) diagonalTopRight NOT set && (1st, 2nd) diagonalDownLeft set')
-            alert(`${currPlayer} WON DIAGONAL LEFT`)
+            return true
           }
         }
         if (currArr.includes(tmpField + 6) && currArr.includes(tmpField + 12) && currArr.includes(tmpField + 18)) {
           console.log('(1st) diagonalTopRight NOT set && (1st, 2nd, 3rd) diagonalDownLeft set')
-          alert(`${currPlayer} WON DIAGONAL LEFT`)
+          return true
         }
-        break
+        return false
 
       case 4:
         if (row < 5 && currArr.includes(tmpField + 6)) {
@@ -230,40 +232,40 @@ function App() {
             console.log('(1st) diagonalDownLeft set && (1st) diagonalTopRight set')
             if (currArr.includes(tmpField - 12)) {
               console.log('(1st) diagonalDownLeft set && (1st, 2nd) diagonalTopRight set')
-              alert(`${currPlayer} WON DIAGONAL LEFT`)
+              return true
             }
             if (currArr.includes(tmpField + 12)) {
               console.log('(1st, 2nd) diagonalDownLeft set && (1st) diagonalTopRight set')
-              alert(`${currPlayer} WON DIAGONAL LEFT`)
+              return true
             }
           }
           if (currArr.includes(tmpField + 12) && currArr.includes(tmpField + 18)) {
             console.log('diagonalTopRight NOT set && (1st, 2nd, 3rd) diagonalDownLeft set')
-            alert(`${currPlayer} WON DIAGONAL LEFT`)
+            return true
           }
         }
-        break
+        return false
 
       case 5:
         if (row < 4 && currArr.includes(tmpField + 6) && currArr.includes(tmpField + 12)) {
           console.log(`row == ${row} (row < 4) && (1st, 2nd) diagonalDownLeft set`)
           if (currArr.includes(tmpField - 6)) {
             console.log(`(1st, 2nd) diagonalDownLeft set && (1st) diagonalTopRight set`)
-            alert(`${currPlayer} WON DIAGONAL LEFT`)
+            return true
           }
           if (currArr.includes(tmpField + 18)) {
             console.log(`(1st, 2nd, 3rd) diagonalDownLeft set && (1st) diagonalTopRight NOT set`)
-            alert(`${currPlayer} WON DIAGONAL LEFT`)
+            return true
           }
         }
-        break
+        return false
 
       case 6:
         if (row < 3 && currArr.includes(tmpField + 6) && currArr.includes(tmpField + 12) && currArr.includes(tmpField + 18)) {
           console.log(`row == ${row} (row < 3) && (1st, 2nd, 3rd) diagonalDownLeft set`)
-          alert(`${currPlayer} WON DIAGONAL LEFT`)
+          return true
         }
-        break
+        return false
     }
   }
 
@@ -284,23 +286,23 @@ function App() {
       case 0:
         if (currArr.includes(tmpField + 1) && currArr.includes(tmpField + 2) && currArr.includes(tmpField + 3)) {
           console.log('(1st, 2nd, 3rd) right set')
-          alert(`${currPlayer} WON SIDE`)
+          return true
         }
-        break
+        return false
 
       case 1:
         if (currArr.includes(tmpField + 1) && currArr.includes(tmpField + 2)) {
           console.log('(1st, 2nd) right set')
           if (currArr.includes(tmpField - 1)) {
             console.log('(1st, 2nd) right set && (1st) left set')
-            alert(`${currPlayer} WON SIDE`)
+            return true
           }
           if (currArr.includes(tmpField + 3)) {
             console.log('(1st, 2nd, 3rd) right set && (1st) left NOT set')
-            alert(`${currPlayer} WON SIDE`)
+            return true
           }
         }
-        break
+        return false
 
       case 2:
         if (currArr.includes(tmpField + 1)) {
@@ -309,19 +311,19 @@ function App() {
             console.log('(1st) right set && (1st) left set')
             if (currArr.includes(tmpField + 2)) {
               console.log('(1st, 2nd) right set && (1st) left set')
-              alert(`${currPlayer} WON SIDE`)
+              return true
             }
             if (currArr.includes(tmpField - 2)) {
               console.log('(1st) right set && (1st, 2nd) left set')
-              alert(`${currPlayer} WON SIDE`)
+              return true
             }
           }
           if (currArr.includes(tmpField + 2) && currArr.includes(tmpField + 3)) {
             console.log('(1st, 2nd, 3rd) right set')
-            alert(`${currPlayer} WON SIDE`)
+            return true
           }
         }
-        break
+        return false
 
       case 3:
         if (currArr.includes(tmpField - 1)) {
@@ -342,7 +344,7 @@ function App() {
           console.log('(1st, 2nd, 3rd) right set && (1st) left NOT set')
           alert(`${currPlayer} WON SIDE`)
         }
-        break
+        return false
 
       case 4:
         if (currArr.includes(tmpField - 1)) {
@@ -363,7 +365,7 @@ function App() {
             alert(`${currPlayer} WON SIDE`)
           }
         }
-        break
+        return false
 
       case 5:
         if (currArr.includes(tmpField - 1) && currArr.includes(tmpField - 2)) {
@@ -377,14 +379,14 @@ function App() {
             alert(`${currPlayer} WON SIDE`)
           }
         }
-        break
+        return false
 
       case 6:
         if (currArr.includes(tmpField - 1) && currArr.includes(tmpField - 2) && currArr.includes(tmpField - 3)) {
           console.log('(1st, 2nd, 3rd) left set')
           alert(`${currPlayer} WON SIDE`)
         }
-        break
+        return false
     }
   }
 
@@ -403,54 +405,68 @@ function App() {
 
       }
       if (currArr.includes(tmpField + 7) && currArr.includes(tmpField + 14) && currArr.includes(tmpField + 21)) {
-        alert(`${currPlayer} WON BOTTOM`)
+        return true
       }
     }
+    return false
   }
 
   const setToken = (event: React.MouseEvent<HTMLElement>) => {
     console.log('----------')
     console.log('FUNCTION: setToken')
-    let gridIndex: number = parseInt((event.target as HTMLButtonElement).value)
-    let col: number = gridIndex % 7
-    let row: number = Math.floor(gridIndex / 7)
-    console.log(`Current field: ${gridIndex}`)
-    console.log(`Col: ${col}`)
-    console.log(`Row: ${row}`)
-    row = 5
-    let tmpField: number = 0
-    let currPlayer: Player = Player.Yellow
-    if (count % 2 === 0) {
-      currPlayer = Player.Red
-    }
-    while (row >= 0) {
-      tmpField = (row * 7) + col
-      if (redArr.includes(tmpField) || yellowArr.includes(tmpField)) {
-        row--
+    if (gameActive) {
+      let gridIndex: number = parseInt((event.target as HTMLButtonElement).value)
+      let col: number = gridIndex % 7
+      let row: number = Math.floor(gridIndex / 7)
+      console.log(`Current field: ${gridIndex}`)
+      console.log(`Col: ${col}`)
+      console.log(`Row: ${row}`)
+      row = 5
+      let tmpField: number = 0
+      let currPlayer: Player = Player.Yellow
+      if (count % 2 === 0) {
+        currPlayer = Player.Red
       }
-      else {
-        let color: string = ""
-        if (currPlayer === Player.Red) {
-          updateRedArr(arr => [...arr, tmpField])
-          color = "bg-red-500"
+      while (row >= 0) {
+        tmpField = (row * 7) + col
+        if (redArr.includes(tmpField) || yellowArr.includes(tmpField)) {
+          row--
         }
         else {
-          updateYellowArr(arr => [...arr, tmpField])
-          color = "bg-yellow-500"
+          let color: string = ""
+          if (currPlayer === Player.Red) {
+            updateRedArr(arr => [...arr, tmpField])
+            color = "bg-red-500"
+          }
+          else {
+            updateYellowArr(arr => [...arr, tmpField])
+            color = "bg-yellow-500"
+          }
+          gridRef.current?.children[tmpField].classList.remove("bg-white")
+          gridRef.current?.children[tmpField].classList.add(color)
+          break
         }
-        gridRef.current?.children[tmpField].classList.remove("bg-white")
-        gridRef.current?.children[tmpField].classList.add(color)
-        break
       }
+      console.log(redArr)
+      console.log(yellowArr)
+      if (checkBottom(row, tmpField, currPlayer)) {
+        setGameActive(false)
+        alert('WON BOTTOM')
+      }
+      else if (checkSide(col, tmpField, currPlayer)) {
+        setGameActive(false)
+        alert('WON SIDE')
+      }
+      else if (checkLeftDiagonal(row, col, tmpField, currPlayer)) {
+        setGameActive(false)
+        alert('WON LEFT DIAGONAL')
+      }
+      else if (checkRightDiagonal(row, col, tmpField, currPlayer)) {
+        setGameActive(false)
+        alert('WON RIGHT DIAGONAL')
+      }
+      setCount(count + 1)
     }
-    console.log(redArr)
-    console.log(yellowArr)
-    checkBottom(row, tmpField, currPlayer)
-    checkSide(col, tmpField, currPlayer)
-    checkLeftDiagonal(row, col, tmpField, currPlayer)
-    checkRightDiagonal(row, col, tmpField, currPlayer)
-
-    setCount(count + 1)
   }
 
   return (
